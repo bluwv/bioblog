3eb290ac78e92fc043d75d77842fd825c4def52a
Adrien <adrien.pierre@stratgie.agency>
2024-04-22 21:21:06 +0200

    Add connexion db

--- config/database.php
+++ config/database.php
@@ -1,7 +1,16 @@
-
 <?php
 
 $dbHost = 'localhost';
 $dbName = 'bioblog';
 $dbUser = 'root';
 $dbPassword = 'root';
+
+try {
+	$pdo = new PDO("mysql:host=$dbHost;dbname=$dbName", $dbUser, $dbPassword);
+	$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+	$pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC); // FETCH_ASSOC FETCH_BOTH
+
+	// echo "Connexion réussie !";
+} catch(PDOException $e) {
+	throw new PDOException("Database connection error: " . $e->getMessage());
+}
